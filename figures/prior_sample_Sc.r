f1386bd7c67d087b47a25fd3216cb58c3538489f
mkln <peruzzi@gmail.com>
2024-10-16 13:28:21 -0400

    new figure code

--- figures/prior_sample_Sc.r
+++ figures/prior_sample_Sc.r
@@ -3,6 +3,7 @@ library(tidyverse)
 library(magrittr)
 library(Matrix)
 library(latex2exp)
+library(spiox)
 
 set.seed(4)
 
@@ -25,49 +26,66 @@ Sigma <- diag(sds) %*% Omega %*% diag(sds)
 St <- chol(Sigma)
 S <- t(St)
 
-# spatial
+# referejce location
 xS <- seq(0, 1, length.out=50)
 cooS <- expand.grid(xS, xS)
 nrS <- nrow(cooS)
 cxS <- as.matrix(cooS) + matrix(runif(nrS*2, -1e-5, 1e-5), ncol=2)
 
-xU <- seq(0, 1, length.out=200)
-cooU <- expand.grid(xU, xU)
-cxU <- as.matrix(cooU)
-
-coords <- bind_rows(cooS %>% mutate(type="S"), 
-                    cooU %>% mutate(type="U"))
-
 m <- 50
 
 custom_dag_S <- dag_vecchia(cxS, m)
-custom_dag_U <- dag_vecchia_predict(cxS, cxU, m = m)
 
+# sample at ref locations using vecchia dag GP
 Hlist <- 1:q %>% sapply(\(j)
-                        spiox::daggp_build(rbind(cxS, cxU), c(custom_dag_S, custom_dag_U), 
+                        spiox::daggp_build(cxS, custom_dag_S, 
                                            phi=theta_mat[1,j], sigmasq=theta_mat[2,j], 
                                            nu=theta_mat[3,j], tausq=theta_mat[4,j], 
                                            matern=T, 16)$H
 )
-
-nr <- nrow(cooS) + nrow(cooU)
-  
+nr <- nrow(cooS) 
 V <- matrix(rnorm(nr * q), ncol=q) %*% St
-
-Y <- V
-
+Ys <- V
 save_which <- rep(0, q)
-
 # make q spatial outcomes
-which_process <- rep(0, q)
 for(i in 1:q){
   cat(i, "\n")
-  Y[,i] <- Matrix::solve(Hlist[[i]], V[,i], sparse=T) 
+  Ys[,i] <- Matrix::solve(Hlist[[i]], V[,i], sparse=T) 
 }
 
-df <- data.frame(coords, y=Y) %>% filter(type=="U") %>% 
+# use spiox predictions to make the sample
+# an alternative is to augment the dag with the U section and sample jointly at S and U
+# since that does the same thing as making predictions based on Ys
+
+
+# spiox_predict wants output from a response model
+# here we pretend we have a sample of size 1 (the arrays below)
+# and use a zero beta coefficient matrix
+
+# non reference locations
+xU <- seq(0, 1, length.out=200)
+cooU <- expand.grid(xU, xU)
+cxU <- as.matrix(cooU)
+
+X <- matrix(0, nrow=nrow(cxS), ncol=1)
+X_new <- matrix(0, nrow=nrow(cxU), ncol=1)
+B_arr <- array(0, dim=c(1, q, 1))
+Sigma_arr <- array(0, dim=c(q,q,1))
+Sigma_arr[,,1] <- Sigma
+theta_arr <- array(0, dim=c(4,q,1))
+theta_arr[,,1] <- theta_mat
+
+
+custom_dag_U <- dag_vecchia_predict(cxS, cxU, m = m)
+
+Yu_out <- spiox::spiox_predict(X_new, cxU, Ys, X, cxS, custom_dag_U, B_arr, Sigma_arr, theta_arr, 8)
+
+# plot
+
+df <- cooU %>% cbind(data.frame(y=Yu_out$Y, type="U")) %>%
   pivot_longer(cols=-c(Var1, Var2, type)) %>%
   mutate(name = ifelse(name == "y.1", "Outcome 1", ifelse(name == "y.2", "Outcome 2", "Outcome 3")))
+
 
 ( p2 <- ggplot(df, 
                aes(Var1, Var2, fill=value)) +
