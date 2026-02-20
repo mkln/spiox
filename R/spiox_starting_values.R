d8bd3d5519e66bb8ad4476c60bd4edd10ff41abf
Michele Peruzzi <peruzzi@gmail.com>
2026-02-20 02:35:30 +0000

    update autostart

--- R/spiox_starting_values.R
+++ R/spiox_starting_values.R
@@ -1,67 +1,6 @@
-find_marginal_pars <- function(Y, X, coords, m = 15, nu=0.5, return_full=FALSE) {
-  
-  if(nu==0.5){
-    covfun_name = "exponential_isotropic"
-  } else if(nu==1.5) {
-    covfun_name = "matern15_isotropic"
-  } else {
-    covfun_name = "matern_isotropic"
-    cat("nu smoothness value != c(0.5, 1.5), fitting matern_isotropic\n")
-  }
-  
-  q <- ncol(Y)
-  theta <- matrix(0, nrow=4, ncol=q)
-  
-  cat("Processing ", q, " outcomes... \n")
-  for(j in seq_len(q)){
-    cat(j, " ")
-    y_j  <- Y[, j]
-    suppressWarnings(out  <- GpGp::fit_model(y = y_j, locs = coords,
-                            X = X, m_seq = m, 
-                            covfun_name = covfun_name,
-                            silent = TRUE))
-    # nugget = sigma * tsq
-    # spatial variance = sigma
-    # total = sigma (1+tsq)
-    # alpha (portion of meas error): tsq/(1+tsq)
-    if(covfun_name == "matern_isotropic"){
-      sigmasq <- out$covparms[1]
-      tsq     <- out$covparms[4]
-      alpha   <- tsq / (1+tsq)
-      phi     <- 1 / out$covparms[2]
-      nu      <- out$covparms[3]
-    } else {
-      sigmasq <- out$covparms[1]
-      tsq     <- out$covparms[3]
-      alpha   <- tsq / (1+tsq)
-      phi     <- 1 / out$covparms[2]
-      nu      <- nu
-    }
-  
-    theta[,j] <- c(phi, sigmasq, nu, alpha)
-  }
-  rownames(theta) <- c("phi", "sigmasq", "nu", "alpha")
-  cat("\n")
-  
-  if(!return_full){
-    theta <- theta[-2,]
-  }
-  return(theta)
-}
-
-
 autostart <- function(Y, X, coords, method=c("response", "latent"), m=15, nu=0.5){
   
   method <- match.arg(method)
-  
-  if(nu==0.5){
-    covfun_name = "exponential_isotropic"
-  } else if(nu==1.5) {
-    covfun_name = "matern15_isotropic"
-  } else {
-    covfun_name = "matern_isotropic"
-    cat("nu smoothness value != c(0.5, 1.5), fitting matern_isotropic\n")
-  }
   
   p <- ncol(X)
   q <- ncol(Y)
@@ -90,15 +29,24 @@ autostart <- function(Y, X, coords, method=c("response", "latent"), m=15, nu=0.5
     W <- matrix(0, 1, 1)
   }
   
-  
   cat("Processing ", q, " outcomes... \n")
+  
+  if(nu==0.5){
+    covfun_name = "exponential_isotropic"
+  } else if(nu==1.5) {
+    covfun_name = "matern15_isotropic"
+  } else {
+    covfun_name = "matern_isotropic"
+    message("nu smoothness value != c(0.5, 1.5), fitting matern_isotropic\n")
+  }
+  
   for(j in seq_len(q)){
     cat(j, " ")
     y_j  <- Y[, j]
-    suppressWarnings(out  <- GpGp::fit_model(y = y_j, locs = coords,
+    invisible(capture.output({out  <- GpGp::fit_model(y = y_j, locs = coords,
                                              X = X, m_seq = m, 
                                              covfun_name = covfun_name,
-                                             silent = TRUE))
+                                             silent = TRUE)}))
     # nugget = sigma * tsq
     # spatial variance = sigma
     # total = sigma (1+tsq)
@@ -128,8 +76,6 @@ autostart <- function(Y, X, coords, method=c("response", "latent"), m=15, nu=0.5
       Theta[,j] <- c(phi, nu, alpha)
     }
   }
-  
-  cat("\n")
 
   if(method == "response"){
     out <- list(
@@ -146,7 +92,6 @@ autostart <- function(Y, X, coords, method=c("response", "latent"), m=15, nu=0.5
       Ddiag = Ddiag
     )
   }
-
   
   return(out)
   
