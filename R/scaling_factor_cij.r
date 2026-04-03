7c5f802a6fd24d480aebfca3d687176bb9400232
Michele Peruzzi <peruzzi@gmail.com>
2026-04-03 21:20:28 +0000

    fix2

--- R/scaling_factor_cij.r
+++ R/scaling_factor_cij.r
@@ -3,9 +3,9 @@ scaling_factor_at_zero <- function(S, thetamat, m=20, nmax=1000, matern=TRUE, n_
   
   subs <- round(seq(1, nr, length.out=min(c(nmax, nr))))
   S_sub <- S[subs,]
-  daginfo <- spiox:::dag_vecchia_orig_order(S_sub, m)
+  daginfo <- spiox:::dag_vecchia_o(S_sub, m)
   
-  R <- sfact(daginfo$dag, S_sub, thetamat, matern, n_threads)
+  R <- sfact(daginfo$dag, S_sub[daginfo$order,], thetamat, matern, n_threads)
   C <- R
   C[upper.tri(C)] <- t(R)[upper.tri(R)]
   
